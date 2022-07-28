2cc9b96a9ecd8394e6dd1ff38d71f288715950e0
unknown <you@example.com>
2022-07-27 18:35:14 -0700

    added to js files in models folder

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,6 +5,25 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
+  try {
+    const productData = await Product.findAll(
+      {
+        include: [
+          {
+            model: Category;
+            attributes: ['category_name']
+          },
+          {
+            model: Tag,
+            attributes: ['tag_name']
+          }
+        ]
+      }
+    );
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all products
   // be sure to include its associated Category and Tag data
 });
